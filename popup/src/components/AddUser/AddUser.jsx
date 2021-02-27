472ddf18fc69223b0d83ed965a28696dd249ca3c
Josh Hess <hessj1@aetna.com>
2021-02-26 23:10:09 -0500

    added state

--- popup/src/components/AddUser/AddUser.jsx
+++ popup/src/components/AddUser/AddUser.jsx
@@ -3,10 +3,16 @@ import './AddUser.css';
 
 function AddUser () {
   const [addOpen, setAddOpen] = useState(false);
-
+  const [userInfo, updateUser] = useState({username: undefined, password: undefined, nickName: undefined});
+  const { username, password, nickName } = userInfo;
   const handleButtonClick = () => {
     setAddOpen(!addOpen);
   };
+
+  const handleInput = (e, type) => {
+    updateUser(prev=>({...prev, [type]: e.value}))
+  };
+
   return (
     <div className="add-user-container">
       <button className={`add-user${addOpen? ' open': ''}`} onClick={handleButtonClick}>
@@ -14,11 +20,13 @@ function AddUser () {
       </button>
       {addOpen &&
         <div className="add-user-input">
+          <button className="close">X</button>
           <div className="add-user-row">
-            <input type="text" placeholder="username" />
-            <input type="text" placeholder="password" />
+            <input value={username} type="text" placeholder="username" onInput={e=>handleInput(e, 'username')} />
+            <input value={password} type="text" placeholder="password" onInput={e=>handleInput(e, 'password')} />
           </div>
-          <input type="text" placeholder="name" className="full-width" />
+          <input value={nickName} type="text" placeholder="name" className="add-user-name full-width"  onInput={e=>handleInput(e, 'nickName')} />
+          <button>Add user</button>
         </div>
       }
     </div>);
